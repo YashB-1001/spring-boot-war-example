5c9600a0e9cf6e617a84a1443c60fc16b0dce5f1
YashB-1001 <138874435+YashB-1001@users.noreply.github.com>
2024-06-18 11:09:02 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello-World i am yashwanth ltm";
+        return "Hello-World 9999";
     }
 
 }
